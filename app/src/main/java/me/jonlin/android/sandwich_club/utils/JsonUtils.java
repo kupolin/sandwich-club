b57d5b735351ea20fd0cf47e52c7d136b0c0655e
kupolin <joanthan.lin108@gmail.com>
2020-02-28 18:16:00 -0800

    Starter Code. Java.

---
+++ app/src/main/java/me/jonlin/android/sandwich_club/utils/JsonUtils.java
@@ -0,0 +1,10 @@
+package me.jonlin.android.sandwich_club.utils;
+
+import me.jonlin.android.sandwich_club.model.Sandwich;
+
+public class JsonUtils {
+
+    public static Sandwich parseSandwichJson(String json) {
+        return null;
+    }
+}
